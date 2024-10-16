db85e34127e0475ad413c7c75a72c78f8921259c
Alan Chelko <achelko@gmail.com>
2024-10-16 12:10:48 +0000

    Refactoring with router

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
+
 
 dotenv.config();
 
@@ -9,34 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
-  res.send("Hello there!  GET request called successfully!")
+  res.send('Hello there!  GET request called successfully!');
 })
-
-app.post('/api/products', async (req, res) => {
-  const product = req.body;
-
-  if(!product.name || !product.price || !product.image) {
-    return res.status(400).json({ 
-      success: false, message: "Please provide all fields."
-    });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in Create product: ", error.message)
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-  
-});
-
-
-
 
 
 
